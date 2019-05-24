8c060d7979ffc42daa572a604ed37ddf8c4d283b
NikitaSenyorita <38568177+NikitaSenyorita@users.noreply.github.com>
2019-05-24 20:54:17 +0300

    Refactor

--- lab5t/lab5t/Source.cpp
+++ lab5t/lab5t/Source.cpp
@@ -2,34 +2,41 @@
 #include <time.h>
 #include <conio.h>
 
-const int N = 5; // Мощность множества
+const size_t N = 6; // Мощность множества
+const size_t POWER = 100; // Мощность множества размещаемых элементов
 
 b_r_tree* generate();
 
 int main()
 {
 	srand(time(NULL));
-	b_r_tree a, b, c, d, e, result;
-	a = *generate();
-	b = *generate();
-	c = *generate();
-	d = *generate();
-	e = *generate();
 
+	b_r_tree A, B, C, D, E, temp1, temp2, temp3, result;
 
-	cout << "A:\n" << a << '\n';
-	cout << "B:\n" << b << '\n';
-	cout << "C:\n" << c << '\n';
-	cout << "D:\n" << d << '\n';
-	cout << "E:\n" << e << '\n';
+	// Генерация множеств
+	A = *generate();
+	B = *generate();
+	C = *generate();
+	D = *generate();
+	E = *generate();
 
-	cout << "-----------------------------------------------------\n";
+	// Вывод множеств
+	cout << "A:\n" << A << '\n';
+	cout << "B:\n" << B << '\n';
+	cout << "C:\n" << C << '\n';
+	cout << "D:\n" << D << '\n';
+	cout << "E:\n" << E << '\n';
+	cout << "-----------------------------------------------------\n\n";
 
+	temp1 = A | B;
+	temp2 = C & D;
+	temp3 = temp2 / E;
+	result = temp1 ^ temp3;
 
-	cout << "RESULT = (A | B) ^ (C & D) / E\n\n";
-	result = ((a | b) ^ ((c&d) / e));
-
-	cout << "RESULT:\n" << result;
+	cout << "A | B\n" << temp1;
+	cout << "\nC & D\n" << temp2;
+	cout << "\n(C & D) / E\n" << temp3;
+	cout << "\nRESULT = (A | B) ^ ((C & D) / E)\n" << result;
 
 	_getch();
 
@@ -40,13 +47,9 @@ b_r_tree* generate()
 {
 	b_r_tree* result = new b_r_tree();
 
-	size_t size = N + (rand() % 6) - 3;
 	int key = 0;
-	for (size_t count = 0; count < size;) {
-		key = (rand() % 900) + 100;
-		if (result->add(key))
-			count++;
-	}
+	for (size_t i = 0; i < N; ++i)
+		result->add(rand() % POWER);
 
 	return result;
 }
